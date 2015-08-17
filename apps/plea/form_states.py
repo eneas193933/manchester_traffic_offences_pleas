e7e4cf49d3793c2f3314b1378c0b0263a7a13d9d
Ian George <ian.george@digital.justice.gov.uk>
2015-08-17 11:14:29 +0100

    Updated template paths

--- apps/plea/form_states.py
+++ apps/plea/form_states.py
@@ -10,7 +10,7 @@ class PleaState(StateWithData):
 
 
 class PleaStates(ConditionalFSM):
-    case_stage = PleaState(template="plea/case.html",
+    case_stage = PleaState(template="case.html",
                            form_class=forms.CaseForm,
                            exits_to=["defendant_details[plea_made_by=Defendant]",
                                      "company_details"])
@@ -18,40 +18,40 @@ class PleaStates(ConditionalFSM):
     company_details = PleaState(template="company_details.html",
                                 form_class=forms.CompanyDetailsForm,
                                 exits_to=["company_plea"])
-    company_plea = PleaState(template="plea/plea.html",
+    company_plea = PleaState(template="plea.html",
                              form_class=forms.PleaForm,
                              exits_to=["company_finances[none_guilty=True]",
                                        "company_review"])
-    company_finances = PleaState(template="plea/company_finances.html",
+    company_finances = PleaState(template="company_finances.html",
                                  form_class=forms.CompanyFinancesForm,
                                  exits_to=["company_review"])
-    company_review = PleaState(template="plea/review.html",
+    company_review = PleaState(template="review.html",
                                form_class=forms.ConfirmationForm,
                                exits_to=["company_details",
                                          "company_plea",
                                          "company_finances",
                                          "company_complete"])
-    company_complete = PleaState(template="plea/complete.html")
+    company_complete = PleaState(template="complete.html")
 
     # Defendant path
-    defendant_details = PleaState(template="plea/your_details.html",
+    defendant_details = PleaState(template="your_details.html",
                                   form_class=forms.YourDetailsForm,
                                   exits_to=["defendant_plea"])
-    defendant_plea = PleaState(template="plea/plea.html",
+    defendant_plea = PleaState(template="plea.html",
                                form_class=forms.PleaForm,
                                exits_to=["defendant_finances[none_guilty=True]",
                                          "defendant_review"])
-    defendant_finances = PleaState(template="plea/your_finances.html",
+    defendant_finances = PleaState(template="your_finances.html",
                                    form_class=forms.YourMoneyForm,
                                    exits_to=["defendant_expenses[hardship=True]",
                                              "defendant_review"])
-    defendant_expenses = PleaState(template="plea/your_expenses.html",
+    defendant_expenses = PleaState(template="your_expenses.html",
                                    form_class=forms.YourExpensesForm,
                                    exits_to=["defendant_review"])
-    defendant_review = PleaState(template="plea/review.html",
+    defendant_review = PleaState(template="review.html",
                                  form_class=forms.ConfirmationForm,
                                  exits_to=["defendant_details",
                                            "defendant_plea",
                                            "defendant_finances",
                                            "defendant_complete"])
-    defendant_complete = PleaState(template="plea/complete.html")
+    defendant_complete = PleaState(template="complete.html")
