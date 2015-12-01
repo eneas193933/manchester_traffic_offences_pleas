e309f6019d19b3952a06d3e77a2a3e1adf25a950
Fred Marecesche <fred@froddd.co.uk>
2015-12-01 10:54:51 +0000

    Finish merge
    Update settings paths and create merged migration.
    
    [MAPDEV410]

---
+++ apps/plea/migrations/0017_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('plea', '0011_auto_20150928_1506'),
+        ('plea', '0016_auto_20151125_1601'),
+    ]
+
+    operations = [
+    ]
